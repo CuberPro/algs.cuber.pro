462f17b32b2520f54052caea7b9c3e8f1df7aaec
Yunqi Ouyang <oyyq9999999@163.com>
2016-06-30 00:29:43 +1200

    add access control for login/logout/register

--- config/web/user.php
+++ config/web/user.php
@@ -4,4 +4,5 @@ return [
     'identityClass' => 'app\models\Users',
     'enableAutoLogin' => true,
     'autoRenewCookie' => true,
+    'loginUrl' => 'user/login',
 ];
--- controllers/UserController.php
+++ controllers/UserController.php
@@ -5,12 +5,51 @@ namespace app\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\helpers\Url;
+use yii\filters\AccessControl;
+use yii\web\ForbiddenHttpException;
 use app\models\LoginForm;
 use app\models\SignupForm;
 
 class UserController extends Controller {
 
     public $defaultAction = 'login';
+
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['login', 'signup', 'logout'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['login', 'signup'],
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'allow' => true,
+                        'actions' => ['logout'],
+                        'roles' => ['@'],
+                    ],
+                ],
+                'denyCallback' => [$this, 'accessDenied'],
+            ]
+        ];
+    }
+
+    public function accessDenied($rule, $action) {
+        $request = Yii::$app->request;
+        $u = Url::toRoute([$request->get('u', '/')]);
+        switch ($action->id) {
+            case 'login':
+            case 'signup':
+            case 'logout':
+                $this->redirect($u);
+                return;
+            default:
+                throw new ForbiddenHttpException('You are not allowed to access this page');
+                return;
+        }
+    }
 
     public function actionLogin($u = '/') {
         $request = Yii::$app->request;
