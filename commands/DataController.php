c0e1aaf26489001bef03f90ec8b1f8bb717a0412
Yunqi Ouyang <oyyq9999999@163.com>
2016-07-30 03:10:36 +1200

    add validating and auto correcting algs function to data command

--- commands/DataController.php
+++ commands/DataController.php
@@ -2,7 +2,9 @@
 
 namespace app\commands;
 
+use Exception;
 use yii\console\Controller;
+use yii\helpers\Console;
 use app\models\db\Subsets;
 use app\models\db\Cases;
 use app\models\db\CasesInSubset;
@@ -17,7 +19,7 @@ use phpQuery;
  */
 class DataController extends Controller {
 
-    public $defaultAction = 'import';
+    public $defaultAction = 'validate-algs';
 
     const URL_BASE = 'http://algdb.net';
 
@@ -36,8 +38,36 @@ class DataController extends Controller {
         ],
     ];
 
+    private $wrongCount;
+
+    /**
+     * Validate existing algs case by case, and fix them where applicable
+     *
+     * @param string $cube   id of the cube
+     * @param string $subset name of the subset
+     */
+    public function actionValidateAlgs($cube = null, $subset = null, $subsubset = null) {
+        $subsets = Subsets::find();
+        if (isset($cube)) {
+            $subsets->where(['cube' => $cube]);
+            if (isset($subset)) {
+                if (isset($subsubset)) {
+                    $subset = $subset . ' ' . $subsubset;
+                }
+                $subsets->andWhere(['like', 'name', $subset]);
+            }
+        }
+        $subsets = $subsets->all();
+        foreach ($subsets as $subset) {
+            $this->validateSubset($subset);
+        }
+        printf("Wrong algs count: %d\n", $this->wrongCount);
+    }
+
     /**
      * Import data from algdb.net
+     * 
+     * @param string $conf the subsets data
      */
     public function actionImport($conf) {
         $conf = require $conf;
@@ -57,6 +87,128 @@ class DataController extends Controller {
                 }
                 $this->importCases($cube, $subset, $id);
             }
+        }
+    }
+
+    private function validateSubset(Subsets $subset) {
+        printf("Start validating %s of %s\n", $subset->name, $subset->cube);
+        $casesInSubsets = $subset->getCasesInSubsets()->orderBy('sequence')->all();
+        foreach ($casesInSubsets as $case) {
+            $this->validateCase($case);
+        }
+        echo Console::renderColoredString(
+            sprintf("%%yFinished validating %s of %s%%n\n", $subset->name, $subset->cube)
+        );
+    }
+
+    private function validateCase(CasesInSubset $case) {
+        printf("\tStart validating case: %s\n", $case->name);
+        $algs = AlgsForCase::findAll(['case' => $case->case]);
+        foreach ($algs as $alg) {
+            $this->validateAlg($alg);
+        }
+        printf("\tFinished validating case: %s\n", $case->name);
+    }
+
+    private function validateAlg(AlgsForCase $alg) {
+        printf("\t\tValidating %s ... ", $alg->alg0->text);
+        $case = $alg->case0;
+        $caseStr = $case->state;
+        $size = $case->cube->size;
+        $c = new CubeNNN($size, $caseStr);
+        $candidateAlgoList = [];
+        $algoText = $alg->alg0->text;
+        $candidateAlgoList[] = preg_replace("/^[Uy]([2']|2'|)\s?|^(?![Uy])/", '', $algoText);
+        $candidateAlgoList[] = preg_replace("/^[Uy]([2']|2'|)\s?|^(?![Uy])/", 'U ', $algoText);
+        $candidateAlgoList[] = preg_replace("/^[Uy]([2']|2'|)\s?|^(?![Uy])/", 'U2 ', $algoText);
+        $candidateAlgoList[] = preg_replace("/^[Uy]([2']|2'|)\s?|^(?![Uy])/", "U' ", $algoText);
+        $candidateAlgoList[] = $algoText;
+        $candidateAlgoList = array_values(array_unique($candidateAlgoList));
+
+        foreach ($candidateAlgoList as $candidate) {
+            $finalAlgo = $this->tryAlgo($candidate, clone $c);
+            if ($finalAlgo) {
+                break;
+            }
+        }
+
+        if (!$finalAlgo) {
+            echo Console::renderColoredString("%cneeds manual correction%n\n");
+            do {
+                $algo = $this->prompt('Please enter correction for this algo(enter del to delete it): ');
+                if ($algo == 'del') {
+                    if ($this->deleteAlgForCase($alg) !== false) {
+                        echo Console::renderColoredString("%rAlgo deleted%n\n");
+                        return true;
+                    }
+                }
+                $finalAlgo = $this->tryAlgo($algo, clone $c);
+            } while (!$finalAlgo && $this->confirm("It doesn't work. Try again?", true));
+        }
+
+        if ($finalAlgo) {
+            if (strcmp($finalAlgo, $algoText) === 0) {
+                echo Console::renderColoredString("%gOK%n\n");
+                return true;
+            }
+            if ($this->replaceAlgWithNewOne($alg, $finalAlgo)) {
+                echo Console::renderColoredString(sprintf("%%yreplaced by %%g%s%%n\n", $finalAlgo));
+                return true;
+            }
+        }
+        
+        $this->wrongCount++;
+        echo Console::renderColoredString("%rWrong%n\n");
+        return false;
+    }
+
+    private function deleteAlgForCase(AlgsForCase $alg) {
+        return $alg->delete();
+    }
+
+    private function tryAlgo($algo, CubeNNN $cube) {
+        $algo = new Algorithm($algo);
+        $aufs = ['U', 'U2', "U'"];
+        $cube->apply($algo);
+        if ($cube->isSolved()) {
+            return strval($algo);
+        }
+        // auf check
+        for($i = 0; $i < 3; $i++) {
+            $cube->moveU();
+            if ($cube->isSolved()) {
+                return strval(new Algorithm($algo . ' ' . $aufs[$i]));
+            }
+        }
+        return false;
+    }
+
+    private function replaceAlgWithNewOne(AlgsForCase $algForCase, string $newAlgoText) {
+        $newAlgHash = md5($newAlgoText);
+        $newAlg = Algs::findOne($newAlgHash);
+        $caseId = $algForCase->case0->id;
+        if (!$newAlg) {
+            $newAlg = new Algs([
+                'id' => $newAlgHash,
+                'text' => $newAlgoText,
+            ]);
+        }
+        $newAlgForCase = new AlgsForCase([
+            'alg' => $newAlg->id,
+            'case' => $caseId,
+        ]);
+        $db = Algs::getDb();
+
+        $transaction = $db->beginTransaction();
+        try {
+            $newAlg->save();
+            $algForCase->delete();
+            $newAlgForCase->save();
+            $transaction->commit();
+            return true;
+        } catch (Exception $e) {
+            $transaction->rollBack();
+            throw $e;
         }
     }
 
